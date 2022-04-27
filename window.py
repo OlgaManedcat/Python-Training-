609bdeb0eed7fe1d922d92d02cb33c28dd271324
OlgaManedcat <98811012+OlgaManedcat@users.noreply.github.com>
2022-04-27 20:48:20 +0200

    Create window.py
    Simple blue window

---
+++ window.py
@@ -0,0 +1,7 @@
+from tkinter import*
+window=Tk()
+window.title('Юрченко Ольга')
+window.geometry('500x500')#size
+window.resizable(0,0)
+window["bg"]="#474CCC"#background color setings
+window.mainloop()
